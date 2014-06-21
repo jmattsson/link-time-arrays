3bb223a500c5d26633ba870feac87798e738ca3f
Johny Mattsson <johny.mattsson@gmail.com>
2014-06-21 15:09:25 +1000

    Fixes for IAR.

--- include/lta/lta-iar.h
+++ include/lta/lta-iar.h
@@ -5,23 +5,28 @@
 #ifndef _LTA_IAR_H_
 #define _LTA_IAR_H_
 
+#ifndef __cplusplus
+# error "Due to lack of support for using link time constant in initializers, only C++ is currently supported with IAR."
+#endif
+
 #define _LTA_ALIGNED
 
-#define _LTA_DEFINE(type,name) \
-  type *const name = __segment_begin(_LTA_STRINGIFY(name))
+#define _LTA_STRINGIFY_X(x) _LTA_STRINGIFY(x)
+#define _LTA_IAR_SEGMENT(name) _LTA_STRINGIFY_X(_LTA_CONCAT(seg_,name))
 
-/* The IAR compiler doesn't recognize a segment name within a compilation
- * unit unless it's seen the segment used first, hence the placeholder here. */
+#define _LTA_DEFINE(type,name) \
+  type *const name = (type *)__segment_begin(_LTA_IAR_SEGMENT(name))
+
 #define _LTA_DECLARE(type,name) \
-  extern void *_lta_placeholder@_LTA_STRINGIFY(name); \
+  _LTA_PRAGMA(segment=_LTA_IAR_SEGMENT(name)); \
   extern type *const name
 
 #define _LTA_ITEM(type,name,item_name) \
-  __root type item_name@_LTA_STRINGIFY(name)
+  __root type item_name@_LTA_IAR_SEGMENT(name)
 
 #define _LTA_NUM_ITEMS(type,name) \
-  ((__segment_begin(_LTA_STRINGIFY(name)) \
-    - __segment_end(_LTA_STRINGIFY(name))) \
+  (((char *)__segment_begin(_LTA_IAR_SEGMENT(name)) \
+    - (char *)__segment_end(_LTA_IAR_SEGMENT(name))) \
    / sizeof(type))
 
 #endif
